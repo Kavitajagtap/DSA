e3f820857c20aa96faa5edbbfd5e8a9e1bff1dde
Kavita Jagtap <65773208+Kavitajagtap@users.noreply.github.com>
2022-04-07 17:23:48 +0530

    Update Bubble_sort.py

--- Sorting/Bubble_sort.py
+++ Sorting/Bubble_sort.py
@@ -1,4 +1,13 @@
+'''
+Bubble sort is a sorting algorithm that compares two adjacent elements and swaps them until
+ they are not in the intended order.
+Just like the movement of air bubbles in the water that rise up to the surface, each element of
+ the array move to the end in each iteration. Therefore, it is called a bubble sort.
+Time Complexities:  Best    :	O(n)
+                    Worst   :	O(n2)
+                    Average : 	O(n2)
 
+'''
 
 def bubbleSort(arr):
     for i in range(len(arr)-1):
