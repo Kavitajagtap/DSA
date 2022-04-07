f50e8233e4bdb14d2e909e066b08b79e59ec19b0
Kavita Jagtap <65773208+Kavitajagtap@users.noreply.github.com>
2022-04-07 17:26:23 +0530

    Update Merge_Sort.py

--- Sorting/Merge_Sort.py
+++ Sorting/Merge_Sort.py
@@ -1,3 +1,15 @@
+"""
+Merge Sort is one of the most popular sorting algorithms that is based on the principle of Divide and Conquer Algorithm.
+Here, a problem is divided into multiple sub-problems. Each sub-problem is solved individually.
+ Finally, sub-problems are combined to form the final solution.
+ 
+ Time Complexity : 
+          Best Case Complexity   :     O(n*log n)
+          Worst Case Complexity  :     O(n*log n)
+          Average Case Complexity:     O(n*log n)
+Space Complexity : 
+          The space complexity of merge sort is O(n).
+"""
 
 def mergeSort(arr):
     if len(arr) > 1:
