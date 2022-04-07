2cca41a4dac05be3b8dcda2128e275dfe6634791
Kavita Jagtap <65773208+Kavitajagtap@users.noreply.github.com>
2022-04-07 17:22:08 +0530

    Create Count_Sort.py

---
+++ Sorting/Count_Sort.py
@@ -0,0 +1,12 @@
+
+
+def countSort(a):
+    count = [0]*(max(a)+1)
+    for i in a:
+        count[i] +=1
+    return [i for i in range(len(count)) if count[i] > 0 for j in range(count[i])]
+    
+a = [2,4,6,1,9,5,8,3,2,2,5]
+print(countSort(a))
+
+
